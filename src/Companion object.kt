79105dd3ebfcd7d5a14d602cd6a61769effa8c8b
WaleedAls <swyd16@gmail.com>
2025-02-17 19:57:29 +0300

    Companion object

---
+++ src/Companion object.kt
@@ -0,0 +1,17 @@
+class TemperatureConverter () {
+
+    companion object {
+        fun toCelsius(celsius: Double): Double {
+            return (celsius * 9 / 5) + 32
+        }
+            fun toFahrenheit(fahrenheit: Double): Double {
+                return (fahrenheit - 32) * 5/9
+
+            }
+        fun kelvinToCelsius(kelvin: Double): Double = kelvin - 273.15
+        }
+
+   // init{
+//
+// }
+    }
