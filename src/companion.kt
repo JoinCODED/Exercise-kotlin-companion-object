f14db9de0bbcc26a338a5ab582c783ab006e81e2
spdys <noorii549@yahoo.com>
2025-02-17 19:12:32 +0300

    Finished part 3 and tested the conversions, adjusted the code as needed to fix it

--- src/companion.kt
+++ src/companion.kt
@@ -1,10 +1,17 @@
 class TemperatureConverter {
     companion object {
         fun cToF(x: Int): Int { // °F = °C × (9/5) + 32
-           return x*(9/5)+32
+           return (x*9/5)+32
         }
         fun fToC(y: Int): Int { // °C = (°F - 32) × 5/9
             return (y-32)*5/9
         }
     }
+}
+
+fun main() {
+    val resultInF = TemperatureConverter.cToF(17)
+    val resultInC = TemperatureConverter.fToC(48)
+    println("$resultInF °F")
+    println("$resultInC °C")
 }
