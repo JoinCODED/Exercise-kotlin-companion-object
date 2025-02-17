fa60074b425aefe50965b240e0260352c20592b3
spdys <noorii549@yahoo.com>
2025-02-17 19:05:40 +0300

    Finished step 2 created two methods for converting f to c and c to f

--- src/companion.kt
+++ src/companion.kt
@@ -1,4 +1,10 @@
 class TemperatureConverter {
     companion object {
+        fun cToF(x: Int): Int { // °F = °C × (9/5) + 32
+           return x*(9/5)+32
+        }
+        fun fToC(y: Int): Int { // °C = (°F - 32) × 5/9
+            return (y-32)*5/9
+        }
     }
 }
