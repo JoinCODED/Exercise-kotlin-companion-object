f825db72c5a6c1a9337b4c996ab0d59e0434f014
HAlGhanim <imzaji98@gmail.com>
2025-02-17 14:05:23 +0300

    Done with the task including all 6 conversions

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
+package TemperatureConverter
 fun main() {
-    println("Hello World!")
+    val result = TemperatureConverter.cToF(58.0)
+    val result2 = TemperatureConverter.cToK(0.0)
+    val result3 = TemperatureConverter.kToC(58.0)
+    val result4 = TemperatureConverter.kToF(58.0)
+    val result5 = TemperatureConverter.fToC(58.0)
+    val result6 = TemperatureConverter.fToK(58.0)
+
+    println(result)
+    println(result2)
+    println(result3)
+    println(result4)
+    println(result5)
+    println(result6)
 }
